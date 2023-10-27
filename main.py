f9d2b02090a0868db4ad47541c5891f10b82e70f
Dzintars <dzintars2@gmail.com>
2023-10-27 19:13:07 +0300

    init

---
+++ main.py
@@ -0,0 +1,115 @@
+import asyncio
+from settings import token, authToken, legalEntityUrl, dataFolder
+import uuid, time, jwt, json, time, csv, os.path, requests
+
+session = None
+error_count = 0
+
+def getToken():
+	print('Izgūstu tokenu')
+	global authToken
+	header = {
+		"typ" : "JWT",
+		"alg" : "RS256",
+		"x5c" : token['publicKey']
+	};
+	claimSet = {
+		"sub": token['clientId'],
+		"jti": str(uuid.uuid4()),
+		"iss": token['clientId'],
+		"aud": token['audience'],
+		"exp": str(int(time.time()) + 3600),
+		"nbf": str(int(time.time())), 
+		}
+	jwtToken = jwt.encode(claimSet, token['privateKey'], algorithm='RS256', headers = header)
+	data = {
+		'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+		'client_assertion': jwtToken,
+		'grant_type': 'client_credentials',
+		'client_id': token['clientId'],
+		'client_secret': token['clientSecret']
+	}
+	r = requests.post(
+			token['url'],
+			data = data,
+			headers = {
+				'Content-Type': 'application/x-www-form-urlencoded'	
+			}
+		)
+	if r.status_code == 200 and 'access_token' in r.json():
+		authToken = r.json()['access_token']
+		print('Tokens izgūts')
+	else:
+		print('Nevieksmīgs tokena pieprasījums')
+		print(r.status_code)
+		print(r.text)
+
+def getLegalEntity(regno, history = False, count = 0):
+	global error_count
+	headers = {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer ' + authToken
+	}
+	url = legalEntityUrl + str(regno) + ('/history' if history else '')
+	r = session.get(
+			url,
+			headers = headers
+		)
+	if r.status_code == 200:
+		with open('data/' + regno +'.json', 'wb') as f:
+			f.write(r.content)
+	else:
+		error_count += 1
+		print("%s: %s" % (r.status_code, r.text))
+		if error_count > 50: 
+			print("Sasniegts kļūdu limits")
+			return False
+	if r.status_code == 401:
+		print("%s: %s" % (r.status_code, r.text))
+		time.sleep(5)
+		if headers['Authorization'] == 'Bearer ' + authToken:
+			getToken()
+		if count > 3: return r
+		return getLegalEntity(regno, history, count = count + 1)
+	return r
+
+
+
+
+def main():
+	print('Palaižam datu pieprasījumus')
+	start_time = time.time()
+	speed = time.time()
+	dataRead = 0
+	getToken()
+	if authToken == '': 
+		return False
+	global session
+	session = requests.Session()
+	#with open('public_data/register.csv', 'r', encoding='UTF-8') as csvfile:
+	if 1==1:
+		#csvreader = csv.reader(csvfile)
+		csvreader = csv.reader(open('public_data/register.csv', "r", encoding="UTF-8"), delimiter=";")
+		#fields = next(csvreader)
+		for row in csvreader:
+			try:
+				#data = row[0].split(';')
+				regno = row[0] #data[0]
+				regtype = row[7] #data[7]
+				terminated = row[12] #data[12]
+				if regtype in ('K', 'B', 'U', 'C', 'E') and terminated == '' and os.path.isfile(dataFolder + str(regno) + '.json') == False:
+					dataRead += 1
+					if dataRead > 200000: break
+					if getLegalEntity(regno, True) == False: break
+					if dataRead % 100 == 0:
+						print("----- %s: speed: %s sec/req, total: %s seconds -----" % (dataRead, (time.time() - speed) / 100, time.time() - start_time))
+						speed = time.time()
+			except Exception as e:
+				print(e)
+				print(row)
+	print("--- %s seconds ---" % (time.time() - start_time))
+
+#asyncio.run(main())
+if __name__ == "__main__":
+    main()
